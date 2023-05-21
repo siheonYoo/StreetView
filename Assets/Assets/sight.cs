fb45a81004c3adf6da87457352ca19f8206ac41f
kimdoheon01 <112850683+kimdoheon01@users.noreply.github.com>
2023-05-21 14:05:40 +0900

    1인칭 시점 구현

---
+++ Assets/Assets/sight.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sight : MonoBehaviour
+{
+    public float fbspeed = 30f;
+    public float lrspeed = 30f;
+    Vector3 fb = new Vector3(0, 0, 1);
+    Vector3 lr = new Vector3(0, 1, 0);
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical") * Time.deltaTime;
+        float h = Input.GetAxis("Horizontal") * Time.deltaTime;
+        transform.Translate(fb * v * fbspeed);
+        transform.Rotate(lr * h * lrspeed);
+        
+    }
+}
